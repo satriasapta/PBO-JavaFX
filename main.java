29bb6f1bf1ddb90f1fd1d12c7e30565b83861a2c
Satria Sapta <48681521+satriasapta@users.noreply.github.com>
2021-12-05 15:29:08 +0700

    Main
    Source Code Javafx

---
+++ main.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasjavafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+/**
+ *
+ * @author Saptaa
+ */
+public class main extends Application {
+    
+    @Override
+public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Registrasi");
+        GridPane gridPane = createRegistrationFormPane();
+        addUIControls(gridPane);
+        Scene scene = new Scene(gridPane, 800, 500);	
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    private GridPane createRegistrationFormPane() {
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(40, 40, 40, 40));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+        return gridPane;
+    }
+
+    private void addUIControls(GridPane gridPane) {
+        // Add Header
+        Label headerLabel = new Label("Registrasi");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPane.add(headerLabel, 0,0,2,1);
+        GridPane.setHalignment(headerLabel, HPos.LEFT);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+        // Add Name Label
+        Label nameLabel = new Label("First Name");
+        gridPane.add(nameLabel, 0,1);
+
+        // Add Name Text Field
+        TextField nameField = new TextField();
+        nameField.setPrefHeight(40);
+        gridPane.add(nameField, 1,1);
+
+        Label lastnameLabel = new Label("Last Name");
+        gridPane.add(lastnameLabel, 0, 2);
+
+        TextField lastnameField = new TextField();
+        lastnameField.setPrefHeight(40);
+        gridPane.add(lastnameField, 1, 2);
+        
+        Label emailLabel = new Label("E-mail Address");
+        gridPane.add(emailLabel, 0, 3);
+
+        TextField emailField = new TextField();
+        emailField.setPrefHeight(40);
+        gridPane.add(emailField, 1, 3);
+        
+        Label contact = new Label("Contact No");
+        gridPane.add(contact, 0, 4);
+
+        TextField contactField = new TextField();
+        contactField.setPrefHeight(40);
+        gridPane.add(contactField, 1, 4);
+
+        // Add Password Label
+        Label passwordLabel = new Label("Password");
+        gridPane.add(passwordLabel, 0, 5);
+
+        // Add Password Field
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPrefHeight(40);
+        gridPane.add(passwordField, 1, 5);
+        
+        Label confirmpass = new Label("Confirm Password");
+        gridPane.add(confirmpass, 0, 6);
+
+        // Add Password Field
+        PasswordField confirmpassField = new PasswordField();
+        confirmpassField.setPrefHeight(40);
+        gridPane.add(confirmpassField, 1, 6);
+
+        // Add Submit Button
+        Button registerButton = new Button("Register");
+        registerButton.setPrefHeight(40);
+        registerButton.setDefaultButton(true);
+        registerButton.setPrefWidth(100);
+        gridPane.add(registerButton, 0, 10, 2, 1);
+        GridPane.setHalignment(registerButton, HPos.CENTER);
+        GridPane.setMargin(registerButton, new Insets(20, 0,20,0));
+
+        registerButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(nameField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your name");
+                    return;
+                }
+                if(lastnameField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your last name");
+                    return;
+                }
+                if(passwordField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a password");
+                    return;
+                }
+                if(emailField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your email");
+                    return;
+                }
+                if(confirmpassField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "your password is incorrect");
+                    return;
+                }
+
+                showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Registration Successful!", "Welcome " + nameField.getText());
+            }
+        });
+    }
+
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
